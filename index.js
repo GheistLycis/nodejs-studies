b981f308ebbef18ceea84c301e4136e0737727b4
GheistLycis <bruno.nitro48@gmail.com>
2022-03-31 10:23:57 -0300

    updatw

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ const path = require("path")
 //-EJS USES "VIEWS" AS A DEFAULT NAME FOR STATIC FOLDER, NOT "VIEW"! FOR HELL SAKE...
 app.set("view engine", "ejs")
 
+
+//IMPORTING DATABASE
+const dataBase =
+[
+    [
+        {
+            title: "Post 1",
+            text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione quia nostrum possimus aliquam earum id facere voluptates eaque, consectetur quae cupiditate perspiciatis porro est non maiores! Nemo ducimus quas culpa?",
+            stars: 2
+        },
+        {
+            title: "Post 2",
+            text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione quia nostrum possimus aliquam earum id facere voluptates eaque, consectetur quae cupiditate perspiciatis porro est non maiores! Nemo ducimus quas culpa?"
+        },
+        {
+            title: "Post 3",
+            text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione quia nostrum possimus aliquam earum id facere voluptates eaque, consectetur quae cupiditate perspiciatis porro est non maiores! Nemo ducimus quas culpa?",
+            stars: 5
+        }
+    ],
+    ads = {}
+]
+
+
 //DEFINING STATIC FOLDER IF WITHOUT VIEW ENGINE
 //app.use(express.static(path.join(__dirname, "views")))
 //DEFINING PUBLIC FOLDER
@@ -14,28 +38,17 @@ app.use(express.static(path.join(__dirname, "public")))
 //ROUTES
 app.get("/", (req, res)=>{
     //-EJS is seeing the folders so we don't have to point them out, just the folders/files within it
-    res.render("index", {
+    res.render("home", {
         title: "Basic Project: Home"
     })
 })
 app.get("/posts", (req, res)=>{
-    res.render("posts", {
-        title: "Basic Project: Posts", 
-        posts: [
-            {
-                title: "Post 1",
-                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione quia nostrum possimus aliquam earum id facere voluptates eaque, consectetur quae cupiditate perspiciatis porro est non maiores! Nemo ducimus quas culpa?"
-            },
-            {
-                title: "Post 2",
-                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione quia nostrum possimus aliquam earum id facere voluptates eaque, consectetur quae cupiditate perspiciatis porro est non maiores! Nemo ducimus quas culpa?"
-            },
-            {
-                title: "Post 3",
-                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione quia nostrum possimus aliquam earum id facere voluptates eaque, consectetur quae cupiditate perspiciatis porro est non maiores! Nemo ducimus quas culpa?"
-            }
-        ]
-    })
+    res.render("posts", 
+        {
+            title: "Basic Project: Posts",
+            posts: dataBase[0]
+        }
+    )
 })
 app.use((req, res)=>{
     res.send("Page not found... =/")
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,3 @@
-alert("JS working!")
+let array = [item = {name: "Bruno"}, item2 = 2]
+
+alert(array[array.indexOf("item")])
